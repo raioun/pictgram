29ebd7f0f16cf4421f0b936ad7608bc52202baf1
raioun <barubarosuraimu@yahoo.co.jp>
2019-07-22 10:34:15 +0000

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { in: 8..32 }, format: { with: /\A[a-z0-9]+\z/i }
+  # validates :password_digest, presence: true, format: { with: /\A[a-z\d]{8,100}+\z/i }
   
   has_secure_password
 end
